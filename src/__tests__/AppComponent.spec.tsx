ec632f067d73646ea598070d04a569b2ed936e80
IchidaU <koo.be.koo@gmail.com>
2025-03-30 00:05:20 +0900

    jest及びreact-testing-library導入、makefileでテスト実行、Github Actions CI/CD設定

---
+++ src/__tests__/AppComponent.spec.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+
+describe("title", () => {
+  it("should render title", () => {
+    render(<App />);
+    expect(screen.getByTestId("title")).toHaveTextContent("学習記録アプリ");
+  });
+});
