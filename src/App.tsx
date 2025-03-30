a53da981fe3fade69e166dd15dc1148129b3c64a
IchidaU <koo.be.koo@gmail.com>
2025-03-30 23:59:58 +0900

    add: useとsuspense使用したデータ取得機能実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,53 @@
-import { Button } from "@chakra-ui/react";
+import { Suspense, use } from "react";
+import {
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+
+import { GetRecords } from "./lib/record";
+
+const useRecords = () => {
+  const recordsPromise = GetRecords();
+  return use(recordsPromise);
+};
+
+const RecordList = () => {
+  const records = useRecords();
+
+  return (
+    <TableContainer>
+      <Table variant="simple" data-testid="table">
+        <Thead>
+          <Tr>
+            <Th>Title</Th>
+            <Th>Time</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {records.map((record) => (
+            <Tr key={record.id}>
+              <Td>{record.title}</Td>
+              <Td>{record.time}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
 
 function App() {
   return (
     <>
       <h1 data-testid="title">学習記録アプリ</h1>
-      <Button>PUSH</Button>
+      <Suspense fallback={<div>Loading...</div>}>
+        <RecordList />
+      </Suspense>
     </>
   );
 }
---
+++ src/lib/record.ts
@@ -0,0 +1,17 @@
+import { Record } from "../domain/record";
+import { supabase } from "../utils/supabase";
+
+export async function GetRecords(): Promise<Record[]> {
+  const response = await supabase.from("study-record").select("*");
+  if (response.error) {
+    console.error(response.error);
+    throw new Error(response.error.message);
+  }
+
+  const recordsData = response.data.map((record) => {
+    return new Record(record.id, record.title, record.time);
+  });
+
+  console.log(recordsData);
+  return recordsData;
+}
