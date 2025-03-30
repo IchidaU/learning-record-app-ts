0eb1bdae5ce380ee6a5170ae3110d7301d48e98c
IchidaU <koo.be.koo@gmail.com>
2025-03-30 09:19:03 +0900

    Chakra UI v2導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@chakra-ui/react";
+
 function App() {
   return (
     <>
       <h1 data-testid="title">学習記録アプリ</h1>
+      <Button>PUSH</Button>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
