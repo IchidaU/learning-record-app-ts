c0db0300ca514912d8fc01b4b7f00b90687e9155
IchidaU <koo.be.koo@gmail.com>
2025-04-02 01:12:12 +0900

    fix: タイトルにスタイル適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { Heading } from "@chakra-ui/react";
 
 import { RecordList } from "./components/RecordList";
 
 function App() {
   return (
     <>
-      <h1 data-testid="title">学習記録アプリ</h1>
+      <Heading as="h1" data-testid="title">
+        学習記録アプリ
+      </Heading>
       <ErrorBoundary fallback={<h2>データ取得に失敗しました。</h2>}>
         <Suspense fallback={<h2>Loading...</h2>}>
           <RecordList />
