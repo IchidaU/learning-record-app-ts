ed7833c18c077f066cd1356ff6031037ae87c3fb
IchidaU <koo.be.koo@gmail.com>
2025-04-02 00:43:08 +0900

    fix: fallbackの値をdivからh2に変更

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <>
       <h1 data-testid="title">学習記録アプリ</h1>
-      <ErrorBoundary fallback={<div>データ取得に失敗しました。</div>}>
-        <Suspense fallback={<div>Loading...</div>}>
+      <ErrorBoundary fallback={<h2>データ取得に失敗しました。</h2>}>
+        <Suspense fallback={<h2>Loading...</h2>}>
           <RecordList />
         </Suspense>
       </ErrorBoundary>
