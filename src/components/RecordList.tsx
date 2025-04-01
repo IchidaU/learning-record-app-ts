9134f1f656996dde479bccab2d879f2796604ddc
IchidaU <koo.be.koo@gmail.com>
2025-04-02 01:05:41 +0900

    fix: 表示されないためエラー検証

--- src/components/RecordList.tsx
+++ src/components/RecordList.tsx
@@ -11,7 +11,15 @@ import {
 import { useFetchData } from "../hooks/useFetchData";
 
 export const RecordList = () => {
-  const { records } = useFetchData();
+  const { records, loading, error } = useFetchData();
+
+  if (loading) {
+    return <div>読み込み中...</div>;
+  }
+
+  if (error) {
+    return <div>エラーが発生しました: {error.message}</div>;
+  }
 
   if (!records || records.length === 0) {
     return <div>データがありません</div>;
--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -5,24 +5,34 @@ import { Record } from "../domain/record";
 export const useFetchData = () => {
   const [records, setRecords] = useState<Record[]>([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
+    console.log("useEffect in useFetchData is running");
     const getAllRecords = async () => {
       try {
+        console.log("fetching data");
         const recordsData = await GetRecords();
+        console.log("Records fetched", recordsData);
         setRecords(recordsData);
         setLoading(false);
       } catch (error) {
-        console.error(error);
+        console.error("Error fetching records:", error);
+        setError(error instanceof Error ? error : new Error(String(error)));
       }
+      setLoading(false);
     };
 
     getAllRecords();
   }, []);
 
-  if (loading) {
-    throw new Promise((resolve) => setTimeout(resolve, 1000));
-  }
+  //   if (error) {
+  //     throw error;
+  //   }
 
-  return { records };
+  //   if (loading) {
+  //     throw new Promise((resolve) => setTimeout(resolve, 1000));
+  //   }
+
+  return { records, loading, error };
 };
