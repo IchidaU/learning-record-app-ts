02f6905b079ca539e07a283eed716044f10ac800
IchidaU <koo.be.koo@gmail.com>
2025-04-10 23:32:24 +0900

    add: フォームのバリデーションと登録後初期化実装

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -13,7 +13,12 @@ import {
   NumberInput,
   NumberInputField,
   Button,
+  FormErrorMessage,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
 } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
 
 import { AddRecord } from "../lib/record";
 import { refetchRecords } from "../hooks/useFetchData";
@@ -22,8 +27,18 @@ type InputFormProps = {
   isOpen: boolean;
   onClose: () => void;
 };
+type FormInputs = {
+  title: string;
+  time: number;
+};
 
 export const InputForm = ({ isOpen, onClose }: InputFormProps) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<FormInputs>();
   const [title, setTitle] = useState("");
   const [time, setTime] = useState("");
 
@@ -34,8 +49,7 @@ export const InputForm = ({ isOpen, onClose }: InputFormProps) => {
   const onClickAdd = () => {
     try {
       AddRecord(title, time);
-      setTitle("");
-      setTime("");
+      reset({ title: "", time: 0 });
       onClose();
       refetchRecords();
     } catch (error) {
@@ -50,18 +64,59 @@ export const InputForm = ({ isOpen, onClose }: InputFormProps) => {
         <ModalHeader>登録画面</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <FormControl>
-            <FormLabel>学習内容</FormLabel>
-            <Input type="text" value={title} onChange={onChangeTitle} />
-            <FormLabel>学習時間</FormLabel>
-            <NumberInput min={0} value={time} onChange={onChangeTime}>
-              <NumberInputField />
-            </NumberInput>
-          </FormControl>
+          <form id="inputForm" onSubmit={handleSubmit(onClickAdd)}>
+            <FormControl isInvalid={Boolean(errors.title)}>
+              <FormLabel htmlFor="title">学習内容</FormLabel>
+              <Input
+                id="title"
+                {...register("title", { required: "内容の入力は必須です" })}
+                type="text"
+                value={title}
+                onChange={onChangeTitle}
+              />
+              <FormErrorMessage>
+                {errors.title && errors.title.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl isInvalid={Boolean(errors.time)}>
+              <FormLabel htmlFor="time">学習時間</FormLabel>
+              <NumberInput
+                id="time"
+                min={0}
+                max={1000}
+                value={time}
+                onChange={onChangeTime}
+              >
+                <NumberInputField
+                  {...register("time", {
+                    required: "時間の入力は必須です",
+                    validate: (value) => {
+                      if (value < 0) {
+                        return "時間は0以上である必要があります";
+                      }
+                    },
+                  })}
+                />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+                <FormErrorMessage>
+                  {errors.time && errors.time.message}
+                </FormErrorMessage>
+              </NumberInput>
+            </FormControl>
+          </form>
         </ModalBody>
         <ModalFooter>
           <Button onClick={onClose}>キャンセル</Button>
-          <Button onClick={onClickAdd} colorScheme="blue" mr={3}>
+          <Button
+            type="submit"
+            form="inputForm"
+            isLoading={isSubmitting}
+            colorScheme="blue"
+            mr={3}
+          >
             登録
           </Button>
         </ModalFooter>
